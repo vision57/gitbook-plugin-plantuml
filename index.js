ed41811b3466208d3ad267dc51ccdbd78e395fe4
Justin Wu <justin.wu@ringcentral.com>
2018-09-23 14:17:03 +0800

    Cache image to reduce generate same image.

--- index.js
+++ index.js
@@ -6,43 +6,81 @@ var re = /^```uml((.*\n)+?)?```$/im;
 var path = require('path');
 
 var promises = [];
+var md5List = [];
 
 module.exports = {
     hooks: {
         // This is called before the book is generated
         "init": function() {
             console.log("init gitbook-plugin-plantuml!");
-            umlPath = path.join(this.options.output, 'assets', 'images', 'uml');
+            cachedPath = path.join('./assets', 'images', 'uml'); 
+            umlPath = path.join(this.options.output, 'assets', 'images', 'uml'); // should same as above
             mkdirp.sync(umlPath);
+            mkdirp.sync(cachedPath);
         },
         // Before parsing markdown
         "page:before": function(page) {
             var content = page.content;
+
+            console.log("page:before gitbook-plugin-plantuml!");
 
             while ((match = re.exec(content))) {
                 var rawBlock = match[0];
                 var umlBlock = match[1];
 
                 var md5 = crypto.createHash('md5').update(umlBlock).digest('hex');
+                var cachedUmlFile = path.join(cachedPath, md5 + '.uml');
                 var umlFile = path.join(umlPath, md5 + '.uml');
-                fs.writeFileSync(umlFile, match[1], 'utf8');
+                
+                md5List.push(md5)
 
-                promises.push(new Promise(function (resolve, reject) {
-                    var gen = plantuml.generate(umlFile,
-                    { format: 'png' },
-                    function (chunk, data) {
-                        if (data) resolve();
-                    });
-                    gen.out.pipe(fs.createWriteStream(umlFile.replace('.uml', '.png')));
-                }));
+                var lastMd5File = ''
+                try {
+                    if (fs.existsSync(cachedUmlFile)) {
+                        console.log('puml cachedUmlFile exist: ' + cachedUmlFile);
+                        lastMd5File = fs.readFileSync(cachedUmlFile);
+                    }
+                }
+                catch (e) {
+                }
 
-                var svgTag = ['![](', '/assets/images/uml/', md5, '.png)'].join('');
-                page.content = content = content.replace(rawBlock, svgTag);
+                if (lastMd5File == umlBlock) {
+                    var svgTag = ['![](', '/assets/images/uml/', md5, '.png)'].join('');
+                    page.content = content = content.replace(rawBlock, svgTag);
+                    //console.log('gitbook-plugin-plantuml same uml: ' + md5 + '.uml');
+                } else {
+                    fs.writeFileSync(cachedUmlFile, umlBlock, 'utf8');
+                    //console.log('gitbook-plugin-plantuml start generate uml: ' + cachedUmlFile);
+                    promises.push(new Promise(function (resolve, reject) {
+                    var gen = plantuml.generate(cachedUmlFile,
+                        { format: 'png' },
+                        function (chunk, data) {
+                            if (data) resolve();
+                        });
+                        var cachePngFile = cachedUmlFile.replace('.uml', '.png');
+                        var pngFile = umlFile.replace('.uml', '.png');
+                        //console.log('gitbook-plugin-plantuml generate success: ' + cachedUmlFile);
+                        gen.out.pipe(fs.createWriteStream(cachePngFile));
+                        gen.out.pipe(fs.createWriteStream(pngFile))
+                    }));
+
+                    var svgTag = ['![](', '/assets/images/uml/', md5, '.png)'].join('');
+                    page.content = content = content.replace(rawBlock, svgTag);
+                }
+
             }
-
             return page;
         },
         "finish": function() {
+            var list = fs.readdirSync(cachedPath)
+            list.forEach(function(file) {
+                fileName = file.substring(0, file.lastIndexOf('.'))
+                if (!md5List.includes(fileName)) {
+                    var oldFile = cachedPath + "/" + file
+                    //console.log('gitbook-plugin-plantuml delete old file: ' + oldFile);
+                    fs.unlinkSync(oldFile);
+                }
+            })
             return Promise.all(promises);
         }
     }
